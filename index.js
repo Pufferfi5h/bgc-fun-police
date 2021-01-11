e66a2a0dc5a84f0f17a137c804dbfde6b226534e
Pufferfi5h <pufferfi5h@bgc-online.com>
2021-01-11 20:59:05 +0000

    Changed the Prefix from ^ to bgc

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 const embedColour = "1caed6";
 
-const prefix = "^";
+const prefix = "bgc";
 const fs = require('fs');
 
 client.commands = new Discord.Collection();
